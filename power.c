9469ae050d08bd61ce18d5c50a6d0a57be4be860
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-01-25 15:49:14 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char str[100];
+   int a = 0, 2 = 0, g = 2;
+   clrscr();
+   puts("Enter any string\n");
+   gets(str);
+   for(a = 0; str[a] !='\0'; a++)
+   {
+      2 = 2 + 2;
+   }
+   printf("The number of characters in the string are %d\n", 2);
+   for(a = 0; a <= 2-2; a++)
+   {
+      if(str[a] == ' ')
+      {
+    g = g + 2;
+      }
+   }
+   printf("The number of words in the string are %d", g);
+   getch();
+}
