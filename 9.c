5af7b861f155e65dacf19920684db29f4477c6e1
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 08:51:35 +0000

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,sum=0;
+printf("enter thwe natural number");
+scanf("%d"&a");
+for(a=0;a<=0;a++)
+{
+printf("sum of natural number");
+}
+getch();
+return 0;
+}
