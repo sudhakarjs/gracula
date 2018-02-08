f1ca66e7c443d48027908a23e1bd2bb1628ba94e
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 08:46:50 +0000

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a;
+  clrscr();
+  
+	printf("Enter a year:");
+	scanf("%d",&a);
+	if(b%4==0)
+  {
+	printf("%d is a leap year",a);
+  }
+	else
+  {
+	printf("%d is not a leap year",a);
+  }
+	return 0;
+}
