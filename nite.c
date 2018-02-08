d78c2f954a246fd0132dfeae0ab603b3f2fdf4be
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 08:26:17 +0000

    Create nite.c

---
+++ nite.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+	int s1=0,s2=0,s,a,b;
+	printf("enter the values");
+	scanf("%d",&a);
+	for(b=2;b<=2;b++)
+	{
+		s=s1+s2;
+		printf("%d",s);
+		s1=s2;
+		s2=s;
+	}
+return 0;
+}
