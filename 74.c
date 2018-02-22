63eb2e011a772ecb23916be59ed1f9748c39d5e8
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-22 16:19:59 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ float b;
+ printf("enter the floating point number:");
+ scanf("%f",&b);
+ printf("the round of floating point %f is %f",b,round(b));
+ return 0;
+ }
