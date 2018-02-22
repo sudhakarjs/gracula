a36d608a30af9c009d56677a0f8ef4f048e8073d
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-22 16:31:33 +0530

    Create 77.c

---
+++ 77.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a,b=0,i;
+printf("enter the number");
+scanf("%d",&a);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+ b++;
+ }        
+}
+if(b==2)
+{
+printf("prime number");
+}
+else
+{
+printf("composite number");
+}
+}
