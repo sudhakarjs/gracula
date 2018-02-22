9de0493f6226cdb5500bbaf5c459d91261d0e5f4
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-22 16:09:41 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char ch[100],a[100];
+gets(ch);
+strcpy(a,ch);
+strrev(a);
+if(strcmp(a,c)==0)
+printf("yes");
+else
+printf("no");
+return 0;
+}
