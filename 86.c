769af78162f998da57bfe7b16c6022a365fd1c57
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-03-08 16:37:52 +0530

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+int a[20],n,k,i,flag=0;
+char c[20];
+scanf("%s",c);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]==s[i+1])
+    flag=1;
+}
+if(flag==1)
+printf("\nNO");
+else
+printf("\n YES");
+return 0;
+}
