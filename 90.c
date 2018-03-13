f2990a8ee3cff0635c3c89e6d44710bccc095fb8
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-03-13 16:12:29 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	char c[10];
+	int i;
+	scanf("%s",c);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(c[i]>='0' && c[i]<='9')
+		{
+			printf("%c",c[i]);
+		}
+	}
+	return 0;
+}
